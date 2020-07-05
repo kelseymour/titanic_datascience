3844fa42f48b579c43e94e584047e810f7443083
Kelsey Seymour <kelseymour@Kelseys-MacBook-Air.local>
2020-07-04 17:13:30 -0700

    Add data pipeline and command line tool to launch it

--- setup.py
+++ setup.py
@@ -20,9 +20,9 @@ setup(
         'Programming Language :: Python :: 3',
     ],
     # Substitute <github_account> with the name of your GitHub account
-    url='https://github.com/catshowjudge/titanic_datascience',
-    author='Kelsey Seymour',  # Substitute your name
-    author_email='kelseymour@gmail.com',  # Substitute your email
+    url='https://github.com/<github_account>/titanic_datascience',
+    author='Filippo Bovo',  # Substitute your name
+    author_email='filippo@satalia.com',  # Substitute your email
     license='MIT',
     packages=['titanic'],
     install_requires=[
@@ -33,7 +33,13 @@ setup(
         'scipy>=1.2.1',
         'matplotlib>=3.0.3',
         'pytest>=4.3.1',
-        'pytest-runner>=4.4'],
+        'pytest-runner>=4.4',
+        'click>=7.0'
+    ],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest']
+    tests_require=['pytest'],
+    entry_points='''
+        [console_scripts]
+        titanic_analysis=titanic.command_line:titanic_analysis
+    '''
 )
